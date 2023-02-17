6dcd1a1d9189e1cecc6022d038ef1cac91551083
Simon LEONARD <git-1001af4@sinux.sh>
2023-02-17 19:50:29 +0000

    add order processor
    thanks to https://itnext.io/overwrite-previously-printed-lines-4218a9563527

---
+++ order_processor.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+import os
+import json
+import boto3
+
+
+def get_args():
+    """Parse command line arguments"""
+    parser = ArgumentParser(description="Process orders from store's queue")
+    parser.add_argument(
+        "STACK_NAME", help="CloudFormation stack name to pull information from", type=str)
+    parser.add_argument("STORE_NUMBER",
+                        help="Store number to process", type=int)
+    return parser.parse_args()
+
+
+def get_stack(stack_name):
+    """ Get CloudFormation stack information 
+    :param stack_name: CloudFormation stack name
+    :return: CloudFormation stack information
+    """
+    return boto3.client("cloudformation").describe_stacks(StackName=stack_name)["Stacks"][0]
+
+
+def cf_output(key, stack):
+    """
+    Get an output value from CloudFormation stack
+    :param key: Output key
+    :return: Output value
+    """
+    try:
+        return next(filter(lambda x: x['OutputKey'] == key, stack['Outputs']))['OutputValue']
+    except IndexError:
+        return None
+
+
+def get_orders(queue_url):
+    """Generator to get messages from SQS queue. This is unsed in a for loop
+    :param queue_url: SQS queue URL
+    """
+    sqs = boto3.client("sqs")
+    while True:
+        response = sqs.receive_message(
+            QueueUrl=queue_url,
+            # AttributeNames=["SentTimestamp"],
+            MaxNumberOfMessages=1,
+            # MessageAttributeNames=["All"],
+            WaitTimeSeconds=5,
+        )
+        if "Messages" in response:
+            for message in response["Messages"]:
+                receipt_handle = message["ReceiptHandle"]
+                yield json.loads(message["Body"])
+                sqs.delete_message(QueueUrl=queue_url,
+                                   ReceiptHandle=receipt_handle)
+
+
+def format_order(order):
+    """Format order information to be printed
+    If order is None, return headers
+    :param order: Order information or None
+    :return: Formatted order information
+    """
+    fmt = "| {:^20} | {:^20} | {:^20} | {:^20} | {:^20} |"
+    if order == None:
+        return fmt.format("Order ID", "Customer", "Item Code", "Item Description", "Quantity")
+    for item in order["items"]:
+        return fmt.format(order["id"], order["customer"], item["code"], item["description"], item["quantity"])
+
+
+if __name__ == "__main__":
+    try:
+        args = get_args()
+        stack = get_stack(args.STACK_NAME)
+        queues = json.loads(cf_output("OutputQueuesUrl", stack))
+        store_nb = str(args.STORE_NUMBER)
+
+        print(format_order(None))
+
+        LINE_UP = '\033[1A'
+        LINE_CLEAR = '\x1b[2K'
+
+        if store_nb in queues:
+            for order in get_orders(queues[store_nb]):
+                print(format_order(order))
+                print(" Acknowledge by pressing enter", end="\r")
+                input()  # Wait
+                # delete ack prompt + add ack mark next to order
+                print(LINE_UP, end=LINE_CLEAR)
+                print(LINE_UP, end=LINE_CLEAR)
+                print(format_order(order) + " (acknowledged)")
+        else:
+            print("No queue for store {}".format(store_nb))
+
+    except KeyboardInterrupt:
+        print("Exiting")
