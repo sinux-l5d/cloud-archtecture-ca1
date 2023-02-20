ec5d09bd35d3def3358df74725a889700dd60e3b
Simon LEONARD <git-1001af4@sinux.sh>
2023-02-20 19:08:03 +0000

    teardown script

---
+++ teardown.sh
@@ -0,0 +1,14 @@
+#!usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Check if AWS SAM CLI is installed
+if ! command -v sam &> /dev/null
+then
+    echo "AWS SAM CLI could not be found"
+    exit 1
+fi
+
+sam delete --no-prompts
